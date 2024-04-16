b74d647938375b68352daff94df39de4de779b32
kimminj00 <kiminju0423@gmail.com>
2024-04-16 10:40:00 +0900

    NUS+BSP service

---
+++ examples/ble_peripheral/ble_example24/pca10040/s132/ses/Output/Debug/Obj/ble_app_uart_pca10040_s132/system_nrf52.d
@@ -0,0 +1,21 @@
+Output/Debug/Obj/ble_app_uart_pca10040_s132/system_nrf52.o: \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\system_nrf52.c \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42b/include/stdint.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42b/include/stdbool.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\system_nrf52.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf52_bitfields.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf51_to_nrf52.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf52_name_change.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\compiler_abstraction.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf_erratas.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf51_erratas.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf52_erratas.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf53_erratas.h \
+ C\:\IET10N_Dev__git\modules\nrfx\mdk\nrf91_erratas.h
